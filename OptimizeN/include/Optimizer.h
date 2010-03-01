b1a4c2b48a8795bc1acc478512e3b50a6e46c44c
dglane001 <dglane001@5160d9da-b3a6-11de-af8f-1f60b1e8fe01>
2010-03-01 17:44:26 +0000

    Removed functionality (moved to ConjGradOptimizer)

--- OptimizeN/include/Optimizer.h
+++ OptimizeN/include/Optimizer.h
@@ -14,7 +14,7 @@
 class COptimizer;
 
 // callback function
-typedef BOOL OptimizerCallback(COptimizer *pOpt, void *pParam);
+//typedef BOOL OptimizerCallback(vnl_nonlinear_minimizer *pOpt, void *pParam);
 
 //////////////////////////////////////////////////////////////////////
 // class COptimizer
@@ -30,8 +30,8 @@ public:
 	// destroy the optimizer
 	virtual ~COptimizer();
 
-	// sets the callback function
-	void SetCallback(OptimizerCallback *pCallback, void *pParam = NULL);
+	//// sets the callback function
+	//void SetCallback(OptimizerCallback *pCallback, void *pParam = NULL);
 
 	// the flag to indicate that gradient information should
 	//		be used
@@ -45,32 +45,33 @@ public:
 	//// holds the number of iterations needed for the optimization
 	//int GetIterations() const;
 
-	// holds the final value of the optimization
-	REAL GetFinalValue() const;
+	//// holds the final value of the optimization
+	//REAL GetFinalValue() const;
 
-	// holds the final value of the parameters for the minimum f
-	// const CVectorN<>& 
-	const vnl_vector<REAL>& 
-		GetFinalParameter() const;
+	//// holds the final value of the parameters for the minimum f
+	//// const CVectorN<>& 
+	//const vnl_vector<REAL>& 
+	//	GetFinalParameter() const;
 
-	// function to actually perform the optimization
-	virtual const CVectorN<>& Optimize(const CVectorN<>& vInit) = 0;
+//protected:
+//	// function to actually perform the optimization
+//	virtual const CVectorN<>& Optimize(const CVectorN<>& vInit) { return vInit; } // = 0;
 
 protected:
-	// the objective function over which optimization is to occur
-	vnl_cost_function/*CObjectiveFunction*/ *m_pFunc;
+	//// the objective function over which optimization is to occur
+	//vnl_cost_function/*CObjectiveFunction*/ *m_pFunc;
 
-	// stores the callback info
-	OptimizerCallback *m_pCallbackFunc;
-	void *m_pCallbackParam;
+	//// stores the callback info
+	//OptimizerCallback *m_pCallbackFunc;
+	//void *m_pCallbackParam;
 
 	// holds the value of the objective function at the final point
-	REAL m_finalValue;
+	//REAL m_finalValue;
 
-	// holds the final input parameter to the objective function (same as
-	//		returned by Optimize)
-	vnl_vector<REAL> m_vFinalParam;
-	CVectorN<> m_vFinalParamTemp;
+	//// holds the final input parameter to the objective function (same as
+	////		returned by Optimize)
+	//vnl_vector<REAL> m_vFinalParam;
+	//CVectorN<> m_vFinalParamTemp;
 
 //private:
 //	// flag to indicate whether gradient information should be used

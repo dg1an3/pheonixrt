1739800910a37c9db92302846ecb57ac4e874e84
dglane001 <dglane001@5160d9da-b3a6-11de-af8f-1f60b1e8fe01>
2010-03-01 00:13:35 +0000

    use vnl_cost_function; removing unused members

--- trunk/OptimizeN/include/Optimizer.h
+++ trunk/OptimizeN/include/Optimizer.h
@@ -25,7 +25,7 @@ class COptimizer : public vnl_nonlinear_minimizer
 {
 public:
 	// construct a new COptimizer object
-	COptimizer(CObjectiveFunction *pFunc);
+	COptimizer(vnl_cost_function/*CObjectiveFunction*/ *pFunc);
 
 	// destroy the optimizer
 	virtual ~COptimizer();
@@ -35,49 +35,46 @@ public:
 
 	// the flag to indicate that gradient information should
 	//		be used
-	BOOL UseGradientInfo() const;
-	void SetUseGradientInfo(BOOL bUseGradientInfo);
+	//BOOL UseGradientInfo() const;
+	//void SetUseGradientInfo(BOOL bUseGradientInfo);
 
 	// defines the tolerance for exit from optimization loop
-	REAL GetTolerance() const;
-	void SetTolerance(REAL tol);
+	//REAL GetTolerance() const;
+	//void SetTolerance(REAL tol);
 
-	// holds the number of iterations needed for the optimization
-	int GetIterations() const;
+	//// holds the number of iterations needed for the optimization
+	//int GetIterations() const;
 
 	// holds the final value of the optimization
 	REAL GetFinalValue() const;
 
 	// holds the final value of the parameters for the minimum f
-	const CVectorN<>& GetFinalParameter() const;
+	// const CVectorN<>& 
+	const vnl_vector<REAL>& 
+		GetFinalParameter() const;
 
 	// function to actually perform the optimization
 	virtual const CVectorN<>& Optimize(const CVectorN<>& vInit) = 0;
 
 protected:
 	// the objective function over which optimization is to occur
-	CObjectiveFunction *m_pFunc;
+	vnl_cost_function/*CObjectiveFunction*/ *m_pFunc;
 
 	// stores the callback info
 	OptimizerCallback *m_pCallbackFunc;
 	void *m_pCallbackParam;
-
-	// holds the tolerance for the optimization
-	// REAL m_tolerance;
-
-	// holds the number of iterations for the optimization
-	// int m_nIteration;
 
 	// holds the value of the objective function at the final point
 	REAL m_finalValue;
 
 	// holds the final input parameter to the objective function (same as
 	//		returned by Optimize)
-	CVectorN<> m_vFinalParam;
+	vnl_vector<REAL> m_vFinalParam;
+	CVectorN<> m_vFinalParamTemp;
 
-private:
-	// flag to indicate whether gradient information should be used
-	BOOL m_bUseGradientInfo;
+//private:
+//	// flag to indicate whether gradient information should be used
+//	BOOL m_bUseGradientInfo;
 
 };	// class COptimizer
 

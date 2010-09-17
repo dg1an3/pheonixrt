d15596af0845df4f975218211df05ec7764a8523
dglane001@gmail.com <dglane001@gmail.com@5160d9da-b3a6-11de-af8f-1f60b1e8fe01>
2010-09-17 00:49:56 +0000

    fixed include path

--- RtOptimizer/VOITerm.cpp
+++ RtOptimizer/VOITerm.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2nd Messenger Systems - U. S. Patent 7,369,645
 // $Id: VOITerm.cpp 640 2009-06-13 05:06:50Z dglane001 $
 #include "stdafx.h"
-#include ".\include\voiterm.h"
+#include <VOITerm.h>
 
 #include <UtilMacros.h>
 #include <Structure.h>

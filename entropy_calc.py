trying to fix autoencoder numerical estimate of entropy gradient

entropy_calc.py
@@ -15,7 +15,7 @@ def mass_density(a):
 
 def entropy(probs):
     import math
-    eps = 1e-6
+    eps = 1e-3
     entropy = sum([-p*math.log(p+eps) for p in probs])
     return entropy
 
@@ -27,3 +27,37 @@ print(entropy(mass_density(spike_image)))
 flat_image = np.random.uniform(0.,1.,size=(3,3))
 print(flat_image)
 print(entropy(mass_density(flat_image)))
+
+def num_grad(a, func, delta=1e-4):
+    """
+    evaluates the numeric gradient of a function
+    """
+    f_a = func(a)
+    assert np.isscalar(f_a),  "func does not produce scalar"
+    grad = np.zeros(a.shape)
+    for r in range(a.shape[0]):
+        for c in range(a.shape[1]):
+            d_a = np.copy(a)
+            d_a[r,c] = d_a[r,c] + delta
+            grad[r,c] = (func(d_a) - f_a)/delta
+    return grad
+
+def entropy_grad(a):
+    """
+    """
+    dEntropy_da = num_grad(a, lambda x:entropy(mass_density(x)), delta=1e-4)
+    return dEntropy_da
+
+def entropy_grad_norm(a):
+    """
+    """
+    return np.linalg.norm(entropy_grad(a))
+
+def maximize_entropy(a, lr=0.1, n=10):
+    for _ in range(n):
+        print('start a = ', a, entropy_grad(a))
+        a = a + lr * entropy_grad(a)
+        print('end a = ', a)
+        a = a / sum(sum(a))
+        print('normalized a = ', a)
+        print('a entropy = ', entropy(mass_density(a)))

mnist_autoencoder_v2.py
@@ -2,11 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-use_v2 = True
-if use_v2:
+# need to turn off eager execution, because we are using placeholders
+# tf.compat.v1.disable_eager_execution()
+
+if tf.version.VERSION == '2.0.0':
     import tensorflow_datasets as tfds
-    datasets, info = tfds.load(name='mnist', with_info=True, as_supervised=True)
+    datasets, info = tfds.load(name='mnist', with_info=True)
     mnist  = datasets['train']
+    # tfds.show_examples(info, mnist)
 else:
     from tensorflow.examples.tutorials.mnist import input_data
     mnist=input_data.read_data_sets("/MNIST_data/",one_hot=True)
@@ -19,8 +22,6 @@ num_output=num_inputs
 lr=0.01
 actf=tf.nn.relu
 
-# need to turn off eager execution, because we are using placeholders
-tf.compat.v1.disable_eager_execution()
 X=tf.compat.v1.placeholder(tf.float32,shape=[None,num_inputs])
 initializer=tf.compat.v1.variance_scaling_initializer()
 
@@ -53,12 +54,14 @@ batch_size=150
 num_test_images=10
 
 with tf.compat.v1.Session() as sess:
-    sess.run(init)
+    # sess.run(init)
+    mnist = mnist.batch(batch_size)
+    test_batch = mnist.take(1)
     for epoch in range(num_epoch):
-        
-        num_batches=mnist.train.num_examples//batch_size
+                
+        num_batches=info.splits['train'].num_examples//batch_size
         for iteration in range(num_batches):
-            X_batch,y_batch=mnist.train.next_batch(batch_size)
+            X_batch = mnist.take(1).as_numpy_iterator()
             sess.run(train,feed_dict={X:X_batch})
             
         train_loss=loss.eval(feed_dict={X:X_batch})
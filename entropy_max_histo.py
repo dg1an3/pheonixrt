b0101411b8abdac8be00d12f465ffe45f2dc2209
Derek Lane <Derek.Lane@elekta.com>
2019-12-30 17:58:49 -0600

    plotting image

--- entropy_max_histo.py
+++ entropy_max_histo.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pprint, math, random, inspect
 
 def create_img(sz, fn, dump=True):
@@ -18,7 +19,7 @@ def calc_bins(img, width=.1):
     """
     # min, max = math.min(img), math.max(img)
     inf = 1.e+2
-    bins_at = [-inf] + [(x/8.)-.25 for x in range(12)] + [inf]
+    bins_at = [-inf] + [(x/16.)-.15 for x in range(24)] + [inf]
     return bins_at
 
 def histogram(img, bins_at, dump=True):
@@ -56,15 +57,20 @@ def entropy_for_img(img, bins_at, dump=True):
 
 if __name__ == '__main__':
 
-    sz = 5
-    sq_img = create_img(sz, lambda r,c:uniform_noise()+((r-sz//2)**2 + (c-sz//2)**2)/4., dump=False)
+    sz = 29
+    sq_img = create_img(sz, lambda r,c:uniform_noise()+((r-sz//2)**2 + (c-sz//2)**2)/1101., dump=False)
+
+    plot_img = True
+    if plot_img:
+        plt.imshow([[sq_img[r*sz+c] for c in range(sz)] for r in range(sz)])
+        plt.show()
 
     # create the bins to be reused
     bins_at = calc_bins(sq_img)
     sq_img_entropy = entropy_for_img(sq_img, bins_at, dump=False)
     print("original entropy = {}".format(sq_img_entropy))
 
-    step = 1e-12
+    step = 1e-10
     for brighten in range(1,100):
         bright_img = create_img(sz, lambda r,c:sq_img[r*sz+c]+step*brighten, dump=False)
         bright_img_entropy = entropy_for_img(bright_img, bins_at, dump=False)

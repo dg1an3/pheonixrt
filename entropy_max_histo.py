5943a6d74d59da13023cefad7b1a55d1634fc10c
Derek Lane <Derek.Lane@elekta.com>
2019-12-30 17:50:52 -0600

    entered initial (non-np) numeric gradient; fixed entropy step test -- very small step 1e-12 provides non-monotonic entropy change

--- entropy_max_histo.py
+++ entropy_max_histo.py
@@ -1,32 +1,43 @@
-import pprint, math
+import pprint, math, random, inspect
 
-def create_img(sz, fn):
-    return [[float(fn(c,r)) for c in range(sz)] for r in range(sz)]
+def create_img(sz, fn, dump=True):
+    """
+    creates a square image in the form of a sz*sz element list vector
+    """
+    img = [float(fn(c,r)) for c in range(sz) for r in range(sz)]
+    if dump:
+        print(inspect.getsource(fn))
+        pprint.pprint(img)
+    return img
 
-def flatten(img):
-    return [px for row in img for px in row]
+def uniform_noise(delta=1e-3):
+    return delta*(random.random()-0.5)
 
 def calc_bins(img, width=.1):
-    img = flatten(img)
-    min, max = min(img), max(img)
-    return min, max
+    """
+    """
+    # min, max = math.min(img), math.max(img)
+    inf = 1.e+2
+    bins_at = [-inf] + [(x/8.)-.25 for x in range(12)] + [inf]
+    return bins_at
 
-def histogram(img, bins_at):
-    img = flatten(img)
-    print("histogram: img (flattened) = "); pprint.pprint(img)
-    print("bins_at = "); pprint.pprint(bins_at)
+def histogram(img, bins_at, dump=True):
+    if dump:
+        print("histogram: img (flattened) = "); pprint.pprint(img)
+        print("bins_at = "); pprint.pprint(bins_at)
     this_bin = 0.
     for (lower,upper) in zip(bins_at, bins_at[1:]):
         def frac(px):
             return (px-lower)/(upper-lower)
         high_fractions = list(map(frac, filter(lambda px:lower<=px and px<upper, img)))
-        print(">>>> \thistogram: for {}: high_fractions = ".format((lower,upper))); pprint.pprint(high_fractions)
+        if dump:
+            print(">>>> \thistogram: for {}: high_fractions = ".format((lower,upper))); pprint.pprint(high_fractions)
         this_bin += sum(map(lambda x:1.-x, high_fractions))
         yield ((lower, upper),this_bin)
         this_bin = sum(high_fractions)
 
 def entropy(bins):
-    eps = 1.e-6
+    eps = 1e-10
     bin_sum = sum([count for _,count in bins])
     entropy = 0.0
     for ((lower,upper),count) in bins:
@@ -34,27 +45,31 @@ def entropy(bins):
         entropy += -p*math.log(p)
     return entropy   
 
+def entropy_for_img(img, bins_at, dump=True):
+    img_bins = list(histogram(img, bins_at, dump))
+    img_entropy = entropy(img_bins)
+    if dump:
+        print("image_histo ="); pprint.pprint(img_bins)
+        print("bin sum = {}".format(sum([count for _,count in img_bins]))) 
+        print("img_entropy = {}".format(img_entropy))
+    return img_entropy
+
 if __name__ == '__main__':
-    sz = 3
-    sq_img = create_img(sz, lambda r,c:((r-sz//2)**2 + (c-sz//2)**2)/4.)
-    print("sq_img ="); pprint.pprint(sq_img)
 
-    #TODO: move this to function
-    inf = 1.e+8
-    bins_at = [-inf] + [(x/4.)-.125 for x in range(4)] + [inf]
-    sq_img_bins = list(histogram(sq_img, bins_at))
-    sq_img_entropy = entropy(sq_img_bins)
-    print("sq_image histo ="); pprint.pprint(sq_img_bins)
-    print("bin sum = {}".format(sum([count for _,count in sq_img_bins]))) 
-    print("sq_image entropy = {}".format(sq_img_entropy))
+    sz = 5
+    sq_img = create_img(sz, lambda r,c:uniform_noise()+((r-sz//2)**2 + (c-sz//2)**2)/4., dump=False)
 
-    bright_img = create_img(sz, lambda r,c:sq_img[r][c]+0.1)
-    print("bright_img ="); pprint.pprint(bright_img)
-    bright_img_bins = list(histogram(bright_img, bins_at))
-    bright_img_entropy = entropy(bright_img_bins)
-    print("bright_img histo ="); pprint.pprint(bright_img_bins)
-    print("bin sum = {}".format(sum([count for _,count in bright_img_bins]))) 
-    print("bright_img entropy = {}".format(bright_img_entropy))
+    # create the bins to be reused
+    bins_at = calc_bins(sq_img)
+    sq_img_entropy = entropy_for_img(sq_img, bins_at, dump=False)
+    print("original entropy = {}".format(sq_img_entropy))
+
+    step = 1e-12
+    for brighten in range(1,100):
+        bright_img = create_img(sz, lambda r,c:sq_img[r*sz+c]+step*brighten, dump=False)
+        bright_img_entropy = entropy_for_img(bright_img, bins_at, dump=False)
+        dentropy = (bright_img_entropy - sq_img_entropy)/(step*brighten)
+        print("dentropy = {}\tupdated entropy = {}".format(dentropy, bright_img_entropy))
 
     #test that a uniform image has entropy >>> pixel count
     #test that a dirac image has entropy >>> ???
